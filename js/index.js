c171e27d7a2b9510871fa58851faa6fcd5c63787
PabloGZumaeta <pablomanuelzumaeta@gmail.com>
2022-09-16 23:48:25 -0500

    Acabado parte mobile

--- js/index.js
+++ js/index.js
@@ -5,22 +5,52 @@ const palabras = [
   "tablet" /* 3 - 4 */,
   "perro" /* 4 - 5 */,
   "estrella" /* 5 - 6*/,
-  "pou" /* 6 - 7*/,
+  "Tom" /* 6 - 7*/,
 ];
-
+let intervaloResponsive;
 let palabraOculta = "";
 let cantErrores = 0;
 let cantAciertos = 0;
+let daltonico = false;
 let ahorcado = obtenerClass(".container-img img");
 const btnJugar = obtenerId("jugar-btn");
 const btnLetras = document.querySelectorAll(".container-letras button");
-let colorValid = (t) => {
-  t.style.borderColor = "rgb(115, 255, 0)";
-  t.style.color = "rgb(115, 255, 0)";
+let palabraAdivinar = obtenerClass(".palabra_adivinar");
+
+// Funciones auxiliares
+let colorValid = (t, m) => {
+  if (m === true) {
+    if (daltonico === true) {
+      t.style.color = "yellow";
+    } else {
+      t.style.color = "rgb(115, 255, 0)";
+    }
+  } else {
+    if (daltonico === true) {
+      t.style.borderColor = "yellow";
+      t.style.color = "yellow";
+    } else {
+      t.style.borderColor = "rgb(115, 255, 0)";
+      t.style.color = "rgb(115, 255, 0)";
+    }
+  }
 };
-let colorInvalid = (t) => {
-  t.style.borderColor = "rgb(255, 0, 43)";
-  t.style.color = "rgb(255, 0, 43)";
+let colorInvalid = (t, m) => {
+  if (m === true) {
+    if (daltonico === true) {
+      t.style.color = "slateblue";
+    } else {
+      t.style.color = "rgb(255, 0, 43)";
+    }
+  } else {
+    if (daltonico === true) {
+      t.style.borderColor = "slateblue";
+      t.style.color = "slateblue";
+    } else {
+      t.style.borderColor = "rgb(255, 0, 43)";
+      t.style.color = "rgb(255, 0, 43)";
+    }
+  }
 };
 
 let resetColor = (t) => {
@@ -34,9 +64,8 @@ let resetColor = (t) => {
   }
 };
 
-// console.log(btnJugar);
-// reiniciar();
 disabled();
+
 function obtenerId(id) {
   return document.getElementById(id);
 }
@@ -44,15 +73,23 @@ function obtenerClass(clas) {
   return document.querySelector(clas);
 }
 
+// Eventos
 // Aqui se esta añadiendo el evento del click en jugar
 btnJugar.addEventListener("click", jugar);
+// Recorremos nuestros botones de letras y ejecutamos la funcion " clickLetras"
+for (let i = 0; i < btnLetras.length; i++) {
+  btnLetras[i].addEventListener("click", clickLetras);
+}
 
 // Aqui generamos la funcionalidad  y dinamismo del evento
 function jugar(event) {
+  intervaloResponsive = setInterval(ajustarMobile, 2500);
+  // ajustarMobile();
+  palabraUsada.innerHTML = " ";
+
   active();
   btnJugar.classList.remove("button-start-active");
   btnJugar.disabled = true;
-  let palabraAdivinar = obtenerClass(".palabra_adivinar");
   palabraAdivinar.innerHTML = "";
   let palabraAzar = Math.floor(Math.random() * palabras.length);
   palabraOculta = palabras[palabraAzar];
@@ -63,16 +100,10 @@ function jugar(event) {
   }
 }
 
-// Recorremos nuestros botones de letras y ejecutamos la funcion " clickLetras"
-for (let i = 0; i < btnLetras.length; i++) {
-  btnLetras[i].addEventListener("click", clickLetras);
-}
-
 // Función  ha ejecutar en nuestros botones
 function clickLetras(e) {
   // Nos permite ver donde se esta ejecutando la tecla
   let tecla = e.target;
-  // console.log(e.target);
   tecla.disabled = true;
   tecla.classList.remove("letra-hover");
   // Asumimos que empieza perdiendo
@@ -84,18 +115,20 @@ function clickLetras(e) {
   // Creamos 2 variables y las pasamos a minusculas para comparar el contenido
   let contenido = tecla.innerHTML.toLowerCase();
   let letraComparar = palabraOculta.toLowerCase();
+
   // Empieza el recorrido de comparación
   for (let i = 0; i < letraComparar.length; i++) {
     if (contenido === letraComparar[i]) {
       resultado = true;
       spans[i].innerHTML = contenido;
       cantAciertos++;
-      colorValid(tecla);
+      colorValid(tecla, false);
+      // console.log(daltonico);
     }
   }
 
   if (resultado === false) {
-    colorInvalid(tecla);
+    colorInvalid(tecla, false);
     cantErrores++;
     let source = `./assets/img/ahorcado${cantErrores}.svg`;
     ahorcado.src = source;
@@ -103,6 +136,7 @@ function clickLetras(e) {
       perdiste();
     }
   }
+
   if (cantAciertos === palabraOculta.length) {
     ganaste();
   }
@@ -116,30 +150,34 @@ function perdiste() {
   let img = obtenerClass(".modal-img img");
   img.src = ` ${imgCambio}`;
   const text = obtenerClass(".text span");
-  // console.log(palabraOculta.toLowerCase());
-  text.innerHTML = `Perdiste :( La palabra era ${palabraOculta.toLowerCase()}`;
+  text.innerHTML = `Perdiste :(   La palabra era ${palabraOculta.toLowerCase()}`;
   reiniciar();
 }
+
 function ganaste() {
   let modal = obtenerClass(".modal");
   modal.classList.toggle("modal-screen", false);
 
-  let imgCambio = `./assets/img/AmanecerAgradecido.jpg`;
+  let imgCambio = `./assets/img/ganaste.gif`;
   let img = obtenerClass(".modal-img img");
   img.src = ` ${imgCambio}`;
   const text = obtenerClass(".text span");
   text.innerHTML = `Ganaste!!! eres todo un crack :)`;
   reiniciar();
 }
+
+// Función auxiliar que permite que que inicien la teclas de PC inactivas
 function disabled() {
   for (let i = 0; i < btnLetras.length; i++) {
     btnLetras[i].disabled = true;
     btnLetras[i].classList.remove("letra-hover");
     resetColor(btnLetras[i]);
   }
+  // Para desbloquear el botón jugar
   btnJugar.disabled = false;
 }
 
+// Para activar las teclas
 function active() {
   for (let i = 0; i < btnLetras.length; i++) {
     btnLetras[i].disabled = false;
@@ -148,17 +186,131 @@ function active() {
   }
 }
 
+//!Función auxiliar clave para que se reseteen las propiedades de los botones
 function reiniciar() {
   let reinicio = obtenerId("re-start");
   let modal = obtenerClass(".modal");
-  // modal.classList.toggle("modal-screen");
+
   reinicio.addEventListener("click", () => {
     modal.classList.toggle("modal-screen", true);
-    // console.log(modal.classList.toggle("modal-screen", true));
     cantAciertos = 0;
     cantErrores = 0;
     disabled();
+
+    //* Que el ahorcado incie desde  la imagen cero
     ahorcado.src = "./assets/img/ahorcado0.svg";
     btnJugar.classList.add("button-start-active");
   });
+  // Termino el evento
+
+  let body = document.querySelector("body");
+  let bodyWidth = window.getComputedStyle(body);
+  let width = parseFloat(bodyWidth.getPropertyValue("width"));
+  if (width < 744) {
+    reiniciarMobile();
+  }
+
+  clearInterval(intervaloResponsive);
 }
+
+// FUNCIONES PARA LA VERSIÓN MOBILE
+
+let envio = obtenerId("enviar-palabra");
+let envioPalabra = obtenerId("enviar-contenido");
+let palabraUsada = obtenerClass(".palabras-usadas-mobile");
+let palabraSpanMobile = obtenerClass(".palabras-usadas-mobile span");
+let palabrasRepetidas = new Set();
+
+envio.addEventListener("click", enviarRespuesta);
+
+function enviarRespuesta() {
+  let resultado = false;
+  let span = document.createElement("span");
+  // Seleccionamos los spans generados por jugar
+  const spans = document.querySelectorAll(".palabra_adivinar span");
+
+  const validarTexto = /^[A-Za-zÑñ]+$/;
+
+  if (!palabrasRepetidas.has(envioPalabra.value.toLowerCase())) {
+    if (validarTexto.test(envioPalabra.value)) {
+      for (let i = 0; i < palabraOculta.length; i++) {
+        if (
+          envioPalabra.value.toLowerCase() === palabraOculta[i].toLowerCase()
+        ) {
+          spans[i].innerHTML = envioPalabra.value.toLowerCase();
+          resultado = true;
+          cantAciertos++;
+          colorValid(span, true);
+        }
+      }
+      palabrasRepetidas.add(envioPalabra.value.toLowerCase());
+      if (cantAciertos === palabraOculta.length) {
+        ganaste();
+      }
+      if (resultado === false) {
+        colorInvalid(span, true);
+        cantErrores++;
+        let source = `./assets/img/ahorcado${cantErrores}.svg`;
+        ahorcado.src = source;
+      }
+      span.innerHTML = envioPalabra.value.toLowerCase();
+      palabraUsada.appendChild(span);
+      // console.log("si evalua");
+    }
+
+    if (cantErrores === 4) {
+      perdiste();
+    }
+  }
+  envioPalabra.value = "";
+}
+
+function reiniciarMobile() {
+  btnJugar.classList.remove("button-start-mobile");
+  visibleGameMobile(".palabra_adivinar", "none");
+  visibleGameMobile(".palabras-usadas-mobile", "none");
+  visibleGameMobile(".content-enviar-mobile", "none");
+  visibleGameMobile(".button-enviar-mobile", "none");
+  palabraUsada.innerHTML = "";
+  palabrasRepetidas.clear();
+}
+
+function ajustarMobile() {
+  let body = document.querySelector("body");
+  let bodyWidth = window.getComputedStyle(body);
+  let widthTamanio = parseFloat(bodyWidth.getPropertyValue("width"));
+  visibleGameMobile(".palabra_adivinar", "flex");
+  visibleGameMobile(".palabras-usadas-mobile", "none");
+  visibleGameMobile(".content-enviar-mobile", "none");
+  visibleGameMobile(".button-enviar-mobile", "none");
+  if (widthTamanio < 744) {
+    // Nota: clase que da un display none
+    btnJugar.classList.add("button-start-mobile");
+    visibleGameMobile(".palabra_adivinar ", "flex");
+    visibleGameMobile(".palabras-usadas-mobile", "flex");
+    visibleGameMobile(".content-enviar-mobile", "flex");
+    visibleGameMobile(".button-enviar-mobile", "inline-block");
+  }
+}
+
+function visibleGameMobile(nombre, tipo) {
+  let elemento = document.querySelector(nombre);
+  elemento.style.display = tipo;
+}
+
+// TODO : Modo Daltonico
+let buttonEye = document.querySelector(".ajustes");
+
+buttonEye.addEventListener("click", activarModo);
+
+function activarModo() {
+  let but = buttonEye.classList.toggle("ajustes-active");
+  daltonico = but;
+  console.log(daltonico);
+}
+
+// LEER ESTO
+// let element = document.querySelector(".main");
+// let elementStyle = window.getComputedStyle(element);
+// let elementColor = elementStyle.getPropertyValue("width");
+// console.log(parseFloat(elementColor));
---
+++ js/movil.js
@@ -0,0 +1,12 @@
+// Empezando el código móvil
+
+// const btnStart = document.getElementById("boton-start");
+
+// document.addEventListener("click", clickStart);
+
+// function clickStart() {
+//   const revelarContent = document.querySelector(".container-content-mobile");
+//   btnStart.style.display = "none";
+//   revelarContent.style.display = "flex";
+//   console.log("probando");
+// }
